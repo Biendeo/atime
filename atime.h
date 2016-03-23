33d44fa78fc06e17d6952df821f6252f25c1912a
Thomas Moffet <pantherman1996@gmail.com>
2016-03-24 09:36:38 +1100

    A new small thing I'm making that will make time operations hopefully a lot easier. Just an initial commit right now.

---
+++ atime.h
@@ -0,0 +1,20 @@
+#ifndef ATIME_H
+#define ATIME_H
+
+#ifdef __cplusplus
+#include <ctime>
+
+class atime {
+	public:
+
+
+	private:
+	time_t time;
+};
+
+#else
+
+#endif
+
+
+#endif
---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include "atime.h"
+
+int main(int argc, char *argv[]) {
+
+}
